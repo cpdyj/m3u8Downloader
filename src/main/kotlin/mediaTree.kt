aad99eff84a9a52e4a047e226141e4723e8ec472
iseki <admin@iseki.space>
2020-03-24 19:08:31 +0800

    clear

--- src/main/kotlin/mediaTree.kt
+++
@@ -1,8 +0,0 @@
-sealed class Media(val title: String) {
-    override fun toString() = title
-}
-
-class SingleFile(title: String, val url: String) : Media(title)
-
-class FileList(title: String, val url: String) : Media(title) {
-}
